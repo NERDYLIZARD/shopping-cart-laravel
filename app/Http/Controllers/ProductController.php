68fc79a072b4c55b31d1e050a32a8e4de0604f3f
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-07-04 16:41:27 +0530

    display products from database

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller {
+  public function getIndex() {
+    $products = Product::all();
+    return view('shop.index',['products' => $products]);
+  }
+}
